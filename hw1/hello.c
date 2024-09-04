8ed8afafc7d71bf81669f57feef55d3166fa5d45
Sarah Harrington <sharrin3@nd.edu>
2024-09-04 14:01:09 -0400

    Homework1

---
+++ hw1/hello.c
@@ -0,0 +1,14 @@
+/**********************************
+ * Author Name: Sarah Harrington
+ * Email: sharrin3@nd.edu
+ *
+ * This file contains the example of Hello, World.
+ * ********************************/
+
+#include <stdio.h>
+
+int main(void){
+
+	fprintf( stdout, "Oy with the poodles already\n" );
+	return 0;
+}
